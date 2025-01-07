ae5681142ebf771d828c3458937f23737d25eb7e
kdkim2000 <kdkim2000@gmail.com/tensorflow/docs>
2025-01-08 04:53:16 +0900

    화면에서 수정 하도록 조치함

--- ch08/user-management-system/src/components/UserList.vue
+++ ch08/user-management-system/src/components/UserList.vue
@@ -10,12 +10,47 @@
     >
       <v-icon size="small">mdi-plus</v-icon>
     </v-btn>
-    <v-data-table :items="users" :headers="headers" class="elevation-1">
+    <v-data-table
+      :items="users"
+      :headers="headers"
+      class="elevation-1"
+      density="compact"
+    >
+      <!-- Role column -->
+      <template #item.role="{ item }">
+        <v-select
+          v-model="item.role"
+          :items="roles"
+          @change="updateUser(item)"
+          color="primary"
+          density="compact"
+        ></v-select>
+      </template>
+      <!-- Active column -->
+      <template #item.active="{ item }">
+        <v-switch
+          v-model="item.active"
+          @change="updateUser(item)"
+          color="primary"
+          density="compact"
+        ></v-switch>
+      </template>
+      <!-- Actions column -->
       <template #item.actions="{ item }">
-        <v-btn icon elevation="0" @click="navigateToEdit(item.id)">
+        <v-btn
+          icon
+          elevation="0"
+          density="comfortable"
+          @click="navigateToEdit(item.id)"
+        >
           <v-icon>mdi-pencil</v-icon>
         </v-btn>
-        <v-btn icon elevation="0" @click="deleteUser(item.id)">
+        <v-btn
+          icon
+          elevation="0"
+          density="comfortable"
+          @click="deleteUser(item.id)"
+        >
           <v-icon>mdi-delete</v-icon>
         </v-btn>
       </template>
@@ -30,7 +65,8 @@ import { useRouter } from "vue-router";
 
 const userStore = useUserStore();
 const router = useRouter();
-
+const roles = ["Admin", "Contributor", "Editor", "Viewer"];
+// Define table headers
 const headers = [
   { title: "ID", value: "id", key: "id" },
   { title: "Name", value: "name", key: "name" },
@@ -46,9 +82,10 @@ onMounted(() => {
   userStore.fetchUsers();
 });
 
-// Use computed to track the users in the store
+// Track the users in the store
 const users = computed(() => userStore.users);
 
+// Navigation methods
 const navigateToAdd = () => {
   router.push("/users/add");
 };
@@ -60,4 +97,24 @@ const navigateToEdit = (id) => {
 const deleteUser = async (id) => {
   await userStore.deleteUser(id);
 };
+
+// Toggle active state
+const updateUser = (item) => {
+  console.log(`updateUser: ${item.id}`);
+  userStore.updateUser({ ...item, active: item.active });
+};
 </script>
+<style scoped>
+.v-data-table tbody tr {
+  height: 34px; /* 각 행의 높이 */
+}
+.v-data-table__actions {
+  height: 30px; /* 작업 열의 너비 */
+}
+.v-switch {
+  height: 38px; /* 스위치의 높이 */
+}
+.v-select {
+  height: 38px; /* 스위치의 높이 */
+}
+</style>
